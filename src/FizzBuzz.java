5f8bff3c57b7a3060c82edf30c806c84ef19a6b6
zjoyjin <joyee.jin1977@gmail.com>
2024-09-09 10:03:03 -0400

    FizzBuzz while loop

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -32,34 +32,29 @@ class FizzBuzz {
         }
         int i=1;
         while (i <100){
-            i = getI(i);
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            i++;
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
 
+                System.out.println("Fizz Buzz");
 
+            } else if (divisibleBy3) {
+
+                System.out.println("Fizz");
+
+            } else if (divisibleBy5) {
+
+                System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(i);
+
+            }
+            
+            
         }
-    }
-
-    private static int getI(int i) {
-        boolean divisibleBy3 = i % 3 == 0;
-        boolean divisibleBy5 = i % 5 == 0;
-        i++;
-        // Print our appropriate result.
-        if (divisibleBy3 && divisibleBy5) {
-
-            System.out.println("Fizz Buzz");
-
-        } else if (divisibleBy3) {
-
-            System.out.println("Fizz");
-
-        } else if (divisibleBy5) {
-
-            System.out.println("Buzz");
-
-        } else {
-
-            System.out.println(i);
-
-        }
-        return i;
     }
 }
